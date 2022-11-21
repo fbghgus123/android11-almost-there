6596b49129ccc734aaf8e95cc59988240dc1e1b6
Yu Ho Hyeon <fbghgus123@naver.com>
2022-11-21 23:00:08 +0900

    :sparkles: feat: 알람 관련 클래스 생성
    - Manifest permission 추가
    - 알람 등록 및 삭제 클래스 생성
    - 알람 receiver 생성
    
    Issue #31

---
+++ app/src/main/kotlin/com/woory/almostthere/background/alarm/AlarmReceiver.kt
@@ -0,0 +1,19 @@
+package com.woory.almostthere.background.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent == null) {
+            return
+        }
+        val alarmType = intent.extras?.getInt("ALARM_TYPE")
+        when (alarmType) {
+            AlarmFunctions.PROMISE_READY -> {}
+            AlarmFunctions.PROMISE_START -> {}
+            else -> throw IllegalArgumentException("is not available alarm type")
+        }
+    }
+}
